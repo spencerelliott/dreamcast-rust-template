4f559c5acaac9b88c2982c8a3c366251a91348af
Spencer Elliott <s.elliott123@gmail.com>
2023-02-06 06:25:01 -0600

    Created init script to copy KOS bindings to src

--- init.sh
+++ init.sh
@@ -3,3 +3,4 @@
 # Use this script to initialize your development environment. This will copy the KallistiOS bindings to your
 # src directory, if it exists.
 
+[ -d "kos-rust-bindings" ] && cp kos-rust-bindings/kos.rs src/kos.rs
