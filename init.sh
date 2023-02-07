78040b226062035faeb5e37f5abf6ca1677fc73c
Spencer Elliott <s.elliott123@gmail.com>
2023-02-07 11:24:29 -0600

    Updated example and bindings

--- init.sh
+++ init.sh
@@ -3,4 +3,4 @@
 # Use this script to initialize your development environment. This will copy the KallistiOS bindings to your
 # src directory, if it exists.
 
-[ -d "kos-rust-bindings" ] && cp kos-rust-bindings/kos.rs src/kos.rs
+make -C out/ copy-bindings
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 #![no_std]
 
+pub mod kos;
+
 #[no_mangle]
 pub extern "C" fn main() {
-
+    unsafe {
+        kos::c_raw::println("Hello, Rust!\n");
+    }
 }
