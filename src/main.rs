5dcbc3296d4f75bd7af406a44847bc51b685b818
Spencer Elliott <s.elliott123@gmail.com>
2023-02-09 12:48:00 -0600

    Started work on simple PVR example

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,10 @@ pub mod kos;
 
 #[no_mangle]
 pub extern "C" fn main() {
-    unsafe {
-        kos::c_raw::println("Hello, Rust!\n");
-    }
+    let pvr_params = kos::pvr::PvrInitParams::new()
+        .with_bin_size(kos::pvr::PvrBin::OPAQUE, kos::pvr::PvrBinSize::BINSIZE_16)
+        .with_vertex_size(512);
+
+    //kos::pvr::Pvr::init_defaults();
+    kos::pvr::Pvr::init(pvr_params);
 }
